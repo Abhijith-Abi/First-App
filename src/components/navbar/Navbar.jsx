c166d4bf6bc619467947f3d400354b94fa84d5a1
abhijith12345 <abhijith@steyp.com>
2023-05-17 15:26:32 +0530

    .

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,115 +1,81 @@
 import React, { useState } from "react";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
-import { styled } from "styled-components";
 import logo from "../../assets/logo.svg";
+import "./navbar.css";
 
-function Navbar() {
+const Navbar = () => {
     const [toggleMenu, setToggleMenu] = useState(false);
 
     return (
-        <MainContainer>
-            <Container>
-                <LogoContainer>
+        <div className="gpt3__navbar">
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
                     <img src={logo} alt="logo" />
-                </LogoContainer>
-                <Section>
-                    <List>
-                        <Item>Home</Item>
-                        <Item>What is GPT3?</Item>
-                        <Item>Open AI</Item>
-                        <Item>Case Studies</Item>
-                        <Item>Library</Item>
-                    </List>
-                </Section>
-                <SecontContainer>
-                    <Sign>Sign in</Sign>
-                    <Button>Sign up</Button>
-                </SecontContainer>
-                <MenuIcon className="gpt3__navbar-menu">
-                    {toggleMenu ? <RiMenu3Line /> : <RiMenu3Line />}
-                    {setToggleMenu && <RiCloseLine />}
-                </MenuIcon>
-            </Container>
-        </MainContainer>
+                </div>
+                <div className="gpt3__navbar-links_container">
+                    <p>
+                        <a href="#home">Home</a>
+                    </p>
+                    <p>
+                        <a href="#wgpt3">What is GPT3?</a>
+                    </p>
+                    <p>
+                        <a href="#possibility">Open AI</a>
+                    </p>
+                    <p>
+                        <a href="#features">Case Studies</a>
+                    </p>
+                    <p>
+                        <a href="#blog">Library</a>
+                    </p>
+                </div>
+            </div>
+            <div className="gpt3__navbar-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+            </div>
+            <div className="gpt3__navbar-menu">
+                {toggleMenu ? (
+                    <RiCloseLine
+                        color="#fff"
+                        size={27}
+                        onClick={() => setToggleMenu(false)}
+                    />
+                ) : (
+                    <RiMenu3Line
+                        color="#fff"
+                        size={27}
+                        onClick={() => setToggleMenu(true)}
+                    />
+                )}
+                {toggleMenu && (
+                    <div className="gpt3__navbar-menu_container scale-up-center">
+                        <div className="gpt3__navbar-menu_container-links">
+                            <p>
+                                <a href="#home">Home</a>
+                            </p>
+                            <p>
+                                <a href="#wgpt3">What is GPT3?</a>
+                            </p>
+                            <p>
+                                <a href="#possibility">Open AI</a>
+                            </p>
+                            <p>
+                                <a href="#features">Case Studies</a>
+                            </p>
+                            <p>
+                                <a href="#blog">Library</a>
+                            </p>
+                        </div>
+                        <div className="gpt3__navbar-menu_container-links-sign">
+                            <p>Sign in</p>
+                            <button type="button">Sign up</button>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
     );
-}
+};
 
 export default Navbar;
-const MainContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 2rem 6rem;
-`;
-
-const Container = styled.div`
-    flex: 1;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-`;
-
-const LogoContainer = styled.div`
-    margin-right: 2rem;
-    width: 62px;
-
-    img {
-        display: block;
-        width: 100%;
-    }
-`;
-
-const Section = styled.div``;
-
-const List = styled.ul`
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    align-items: center;
-`;
-
-const Item = styled.li`
-    color: #fff;
-    font-family: var(--font-family);
-    font-weight: 500;
-    font-size: 16px;
-    line-height: 25px;
-    text-transform: capitalize;
-    margin: 0 1rem;
-    cursor: pointer;
-`;
-
-const SecontContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    align-items: center;
-`;
-
-const Sign = styled.p`
-    color: #fff;
-    font-family: var(--font-family);
-    font-weight: 500;
-    font-size: 18px;
-    line-height: 25px;
-    text-transform: capitalize;
-    margin: 0 1rem;
-    cursor: pointer;
-`;
-
-const Button = styled.button`
-    padding: 0.5rem 1rem;
-    color: #fff;
-    background: #ff4820;
-    font-family: var(--font-family);
-    font-weight: 500;
-    font-size: 18px;
-    line-height: 25px;
-    border: none;
-    outline: none;
-    cursor: pointer;
-    border-radius: 5px;
-`;
-const MenuIcon = styled.div`
-    color: #fff;
-`;
